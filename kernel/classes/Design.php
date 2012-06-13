78c66fe1e4ced7755fa6a59c43a93d8d16073d6f
martin-damien <damien@martin-damien.fr>
2012-06-13 23:38:25 +0200

    Adding some documentation about modules

--- kernel/classes/Design.php
+++ kernel/classes/Design.php
@@ -37,7 +37,7 @@ class Design
                     break;
 
                 case 'css':
-                    $html .= "<link rel='stylesheet' type='text/css' href='$file' />"
+                    $html .= "<link rel='stylesheet' type='text/css' href='$file' />";
                     break;
 
             }
